756e6e455522769392ddb12184ba84eee67810b3
Israel dos Santos Candeias <kojimaster33@gmail.com>
2024-09-12 14:28:52 -0300

    add shadow player

--- groups.py
+++ groups.py
@@ -1,10 +1,13 @@
 from settings import *
+from support import import_image
+from entities import Entity
 
 class AllSprites(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
         self.offset = vector()
+        self.shadow = import_image('.', 'graphics', 'shadow', 'shadow')
 
     # camera
     def draw(self, player):
@@ -12,4 +15,6 @@ class AllSprites(pygame.sprite.Group):
         self.offset.y = -(player.rect.centery - WINDOW_HEIGHT / 2)
 
         for sprite in self:
+            if isinstance(sprite, Entity):
+                self.display_surface.blit(self.shadow, sprite.rect.topleft + self.offset + vector(40, 110))
             self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
